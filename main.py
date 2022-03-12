d1409459dfe39856ae2c447bb61cecf392cad0e9
solidus66 <66solidus@gmail.com>
2022-03-12 20:48:10 +0300

    added input & output files

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-orig_list = [1, 2, 6, 5, 6, 8, 6, 5, 7, 3]
-print("\nOriginal list: " + str(orig_list) + "\n")
+def read_list_from_file(filepath):
+    file = open(filepath, 'r')
+    orig_list = [int(x) for x in file.read().strip().split(" ")]
+    file.close()
+    return orig_list
 
-new_list = []
-for element in orig_list:
-    if element not in new_list:
-        new_list.append(element)
 
-print("List after removing duplicates: " + str(new_list))
+def write_list_to_file(filepath, new_list: list):
+    file = open(filepath, 'w')
+    file.write(" ".join([str(x) for x in new_list]))
+    file.close()
+
+
+def remove_duplicates(initial_list: list):
+    new_list = []
+    for element in initial_list:
+        if element not in new_list:
+            new_list.append(element)
+
+    return [new_list]
+
+
+if __name__ == '__main__':
+    write_list_to_file("output/output_1.txt", remove_duplicates(read_list_from_file("input/input_1.txt")))
